312de350efce81080dd1b2d8c92d5414fe92a5cb
luiscarvil <1888@holbertonschool.com>
2020-12-22 16:29:45 -0500

    pack web statics

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,12 @@ from datetime import datetime
 
 
 def do_pack():
+    """Return the archive path if the archive has been correctly generated
+    """
     time_format = strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
     try:
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".format(
+        local("tar -czvf versions/web_static_{:s}.tgz web_static/".format(
               time_format))
         return ("versions/web_static_{:s}.tgz".format(time_format))
     except Exception:
