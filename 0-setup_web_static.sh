62711be4c819e7b09a66f2bced1c1dcd29859dc3
luiscarvil <1888@holbertonschool.com>
2020-12-22 19:58:26 -0500

    add task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # script that sets up your web servers for the deployment of web_static
 sudo apt-get -y update
-sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "Holberton School New" | sudo tee /data/web_static/releases/test/index.html
