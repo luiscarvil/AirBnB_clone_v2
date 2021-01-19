075e652adfaaa24f0e02fb60332521169cfbdb3e
luiscarvil <1888@holbertonschool.com>
2021-01-19 21:50:11 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+    Sript that starts a Flask web application
 """
-
 from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
+def handle_teardown(self):
     """
     handle teardown
     """
@@ -17,13 +16,12 @@ def tear_down(exception):
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
+def state_list():
     """
-    method to render states
+    render states
     """
     states = storage.all('State').values()
     return render_template("7-states_list.html", states=states)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
