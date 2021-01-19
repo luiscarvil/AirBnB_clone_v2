b273d1a5406a532d94f4d802dea9249e0ef76eb4
luiscarvil <1888@holbertonschool.com>
2021-01-19 20:33:56 +0000

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,14 @@ flask class
 """
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def starts_flask():
     """
     display: Hello HBNB
     """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
