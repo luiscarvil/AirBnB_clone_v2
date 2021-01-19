44813f8b1b7bedf401894e760d76bbd1882e60ff
luiscarvil <1888@holbertonschool.com>
2021-01-19 20:49:22 +0000

    web flask

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -56,12 +56,18 @@ def display_int(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_num_temp(n=None):
+    """
+    display number templates
+    """
     if isinstance(n, int):
         return render_template("5-number.html", n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def display_num_odd_even(n):
+    """
+    display if number is odd or even
+    """
     if isinstance(n, int):
         if (n % 2 == 0):
             return render_template("6-number_odd_or_even.html", n=n, eo="odd")
