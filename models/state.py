9dc092ad3370a3b317232049780e21baf53493e5
luiscarvil <1888@holbertonschool.com>
2021-01-19 20:58:35 +0000

    web flask

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
