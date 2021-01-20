91c940222fa444f2d42adeac4f103ff92614d188
luiscarvil <1888@holbertonschool.com>
2021-01-20 16:13:05 +0000

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
@@ -20,8 +21,9 @@ def state_list():
     """
     render states
     """
-    states = storage.all('State').values()
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
+
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
